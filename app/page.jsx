9013a19c9e10073f9a411d922f2c216f151b020a
Amrita Gupta <amamrita0818@gmail.com>
2026-04-01 11:04:17 +0530

    Add animate UI and reusables component

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,11 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { GoldTitle, GrayTitle, SectionLabel } from "@/components/reusables";
 
 export default function Home() {
   return (
-    <div>
-      <section className="pt-28 sm:pt-32">
-        <Button variant="gold" size="hero">
-          Subscribe
-        </Button>
+    <div className="bg-black overflow-x-hidden">
+      <section className="pt-28 sm:pt-32 relative min-h-screen grid grid-cols-1 lg:grid-cols-5 px-4 sm:px-8 pb-20 overflow-hidden">
+        {/* Hero  */}
+        <SectionLabel>Welcome to Intelliview</SectionLabel>
       </section>
     </div>
   );
--- components/header.jsx
+++ components/header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
       <div className="flex items-center gap-3">
         <Show when="signed-out">
             {/* Links  */}
-            
+
             {/* Credits  */}
 
           <SignInButton>
